f0145ce3f5c5696f7cd7c6d1b5820cfc7b976564
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-05-29 12:57:57 +0300

    Create Solution.java

---
+++ Task-D/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Solution {
+	public static void main(String[] args) {
+		final Scanner scanner = new Scanner(System.in);
+		final BigInteger a = scanner.nextBigInteger();
+		final BigInteger b = scanner.nextBigInteger();
+		System.out.println(a.add(b));
+	}
+} 
